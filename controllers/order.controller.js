809c43deaf718c7ddececbe73cefc3c32ca57bf3
hoangdaheo <hoangdaheodaheo@gmail.com>
2021-12-13 16:44:02 +0700

    fix bug 4

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -121,7 +121,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   if (order.pendingTime > Date.now()) {
     return next(new AppError('User is paying!', 400));
   }
-  const timeout = Number.parseInt(new Date().getTime() / 1000 + 5);
+  const timeout = 1000 * 60 * 2;
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     // success_url: `${req.protocol}://${req.get('host')}/?tour=${
