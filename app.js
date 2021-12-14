df3f8a3f890d295ff4919c13d43d617e3b4df5bd
hoangdaheo <hoangdaheodaheo@gmail.com>
2021-12-14 18:14:14 +0700

    Add more security

--- app.js
+++ app.js
@@ -9,20 +9,48 @@ const cookieParser = require('cookie-parser');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/error.controller');
 const cors = require('cors');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const rateLimit = require('express-rate-limit');
 const app = express();
 app.use(cors());
+app.use(helmet());
 // app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.options('*', cors());
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
 // Middleware
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-
+app.use(mongoSanitize());
+app.use(xss());
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
 //Routes
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/products', productRouter);
