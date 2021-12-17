df523896cc82e4ac8e5df327cb993b8ca591dfd3
hoangdaheo <hoangdaheodaheo@gmail.com>
2021-12-17 11:10:19 +0700

    Change the msg

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -167,12 +167,12 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   // 3) Send it to user's email
-  const resetURL = `${req.protocol}://${req.get(
-    'host'
-  )}/api/v1/users/resetPassword/${resetToken}`;
+  // const resetURL = `${req.protocol}://${req.get(
+  //   'host'
+  // )}/api/v1/users/resetPassword/${resetToken}`;
 
-  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
-
+  // const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
+  const message = `Forgot your password? Get the Refresh Token to reset your Password <${resetToken}>`;
   try {
     await sendEmail({
       email: user.email,
@@ -187,10 +187,15 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   } catch (err) {
     user.passwordResetToken = undefined;
     user.passwordResetExpires = undefined;
-    console.log(err);
+    // console.log(err);
     await user.save({ validateBeforeSave: false });
 
-    return next(new AppError(err.message, 500));
+    return next(
+      new AppError(
+        'There was an error sending the email. Try again later!',
+        500
+      )
+    );
   }
 });
 
